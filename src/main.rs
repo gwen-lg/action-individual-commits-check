22ffed2e0a16e7d31930044d8c8aff75f9adb54a
Gwen Lg <me@gwenlg.fr>
2025-01-27 15:23:40 +0100

    create file for GITHUB_OUTPUT at start
    and use write!

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 use std::{
     env,
-    fs::{write, File},
+    fs::File,
+    io::Write,
     process::{exit, Command},
 };
 
 fn main() {
     let github_output_path = env::var("GITHUB_OUTPUT").unwrap();
+    let mut output_file = File::create(github_output_path).expect("Create output file failed");
 
     let args: Vec<String> = env::args().collect();
     let error = &args[1];
 
     if !error.is_empty() {
         eprintln!("Error: {error}");
-        write(github_output_path, format!("error={error}")).unwrap();
+        write!(output_file, "error={error}").unwrap();
         exit(1);
     }
 }
