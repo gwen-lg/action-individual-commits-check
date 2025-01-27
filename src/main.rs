d1acbecbb00e7093dcf10b0e5fa6ff2265137159
Gwen Lg <me@gwenlg.fr>
2025-01-27 18:21:35 +0100

    rework exit code management

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::{
     process::Command,
 };
 
+use anyhow::Ok;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -24,8 +25,11 @@ enum Error {
         cmd: String,
     },
 
-    #[error("Running of check-cmd : {cmd} failed.")]
-    CommandExecutionFailed { cmd: String },
+    #[error("Running of check-cmd : {cmd} failed with exit code {exit_code}.")]
+    CommandExecutionFailed { cmd: String, exit_code: i32 },
+
+    #[error("The process {cmd} did not return any code.")]
+    NoReturnCodeProcess { cmd: String },
 
     #[error("The test error was triggered")]
     TestGhAction,
@@ -65,9 +69,16 @@ fn main() -> anyhow::Result<()> {
         if output.status.success() {
             write!(output_file, "No error").unwrap();
         } else {
+            let exit_code = output
+                .status
+                .code()
+                .ok_or_else(|| Error::NoReturnCodeProcess {
+                    cmd: check_cmd.into(),
+                })?;
             output_file.write_all(&output.stderr).unwrap();
             return Err(Error::CommandExecutionFailed {
                 cmd: check_cmd.into(),
+                exit_code,
             }
             .into());
         }
